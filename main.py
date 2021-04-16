8a3dd2f789adfbd54e157bf4fc10ba3af321dfbd
Bilau Adeniran <>
2021-04-16 09:21:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#Django is a free, open source server-side web framework that enables the rapid development of web applications. With DJango you can build and maintain high quality and scalable web applications with ease
+
+#Django provides support for common web development tasks to make developers focus on the fun side of building web applications rather than spending so much time reinventing the wheel. They include , but not limited to: user authentication, content administration,routint,templates and views,site maps,RSS feeds etc. ehy use Django,it has rapid development,scalability,fast,reassuringly secure
+
+#pip install django
+#pip freeze to check all installed packages
+#django-admin startproject ournewproject ,to start new project in django
+#python manage.py runserver , to run our first server
+#python manage.py startapp ourapp
